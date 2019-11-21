ff566b79e10915ec06afe179abde80480c120294
Lee <18611401994@163.com>
2019-11-21 18:13:41 +0800

    完善震动工具

--- Sources/Utils/Vibrate.swift
+++ Sources/Utils/Vibrate.swift
@@ -1,42 +1,32 @@
 import UIKit
 
-/// 震动
-///
-/// - light: 轻度
-/// - medium: 中度
-/// - heavy: 重度
-/// - select: 选择
-/// - soft: 软
-/// - rigid: 硬
-public enum Vibrate: Int {
-    case light
-    case medium
-    case heavy
+public enum Vibrate {
+    
     case select
     
-    @available(iOS 13.0, *)
-    case soft
-
-    @available(iOS 13.0, *)
-    case rigid
+    case impact(UIImpactFeedbackGenerator.FeedbackStyle)
+    
+    case notification(UINotificationFeedbackGenerator.FeedbackType)
 }
 
 extension Vibrate {
     
     public func play() {
         switch self {
-        case .light, .medium, .heavy, .soft, .rigid:
-            guard let style = UIImpactFeedbackGenerator.FeedbackStyle(rawValue: rawValue) else {
-                return
-            }
-            let generator = UIImpactFeedbackGenerator(style: style)
-            generator.prepare()
-            generator.impactOccurred()
-            
         case .select:
             let generator = UISelectionFeedbackGenerator()
             generator.prepare()
             generator.selectionChanged()
+            
+        case .impact(let value):
+            let generator = UIImpactFeedbackGenerator(style: value)
+            generator.prepare()
+            generator.impactOccurred()
+            
+        case .notification(let value):
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(value)
         }
     }
 }
