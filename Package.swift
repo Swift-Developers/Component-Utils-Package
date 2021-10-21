9c03bed55c3a25e4ce5a762bcd8a7bada6b1f500
fun <lw_fun@163.com>
2021-10-22 01:19:06 +0800

    cache模块

--- Package.swift
+++ Package.swift
@@ -10,15 +10,49 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Utils",
-            targets: ["Utils"]),
+            targets: [
+                "Utils",
+                "Utils-Tools",
+                "Utils-Cache"
+            ]
+        ),
+        .library(
+            name: "Utils-Tools",
+            targets: ["Utils-Tools"]
+        ),
+        .library(
+            name: "Utils-Cache",
+            targets: ["Utils-Cache"]
+        )
     ],
     dependencies: [
+        .package(name: "Disk", url: "https://github.com/saoudrizwan/Disk.git", from: "0.6.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Utils"),
+            name: "Utils",
+            dependencies: [
+                "Utils-Tools",
+                "Utils-Cache"
+            ],
+            path: "Sources",
+            sources: ["Utils"]
+        ),
+        .target(
+            name: "Utils-Tools",
+            path: "Sources",
+            sources: ["Tools"]
+        ),
+        .target(
+            name: "Utils-Cache",
+            dependencies: [
+                .product(name: "Disk", package: "Disk")
+            ],
+            path: "Sources",
+            sources: ["Cache"]
+        ),
         .testTarget(
             name: "UtilsTests",
             dependencies: ["Utils"]),
---
+++ Sources/Cache/Cache.swift
@@ -0,0 +1,152 @@
+import Foundation
+import Disk
+import CommonCrypto
+
+public class Cache {
+    
+    public typealias Directory = Disk.Directory
+    
+    public static let shared = Cache(.caches, "Temp")
+    
+    public let directory: Directory
+    public let path: String
+    
+    public init(_ directory: Directory, _ path: String) {
+        self.directory = directory
+        self.path = path
+    }
+    
+    public func set<T: Encodable>(_ object: T, forKey key: String) throws {
+        do {
+            try Disk.save(object, to: directory, as: path + "/\(key.md5)")
+            
+        } catch {
+            throw Error(rawValue: (error as NSError).code) ?? .unknow
+        }
+    }
+    public func get<T: Decodable>(for key: String) -> Swift.Result<T, Error> {
+        do {
+            let data = try Disk.retrieve(path + "/\(key.md5)", from: directory, as: T.self)
+            return .success(data)
+            
+        } catch {
+            return .failure(Error(rawValue: (error as NSError).code) ?? .unknow)
+        }
+    }
+    
+    public func set(_ data: Data, forKey key: String) throws {
+        do {
+            try Disk.save(data, to: directory, as: path + "/\(key.md5)")
+
+        } catch {
+            throw Error(rawValue: (error as NSError).code) ?? .unknow
+        }
+    }
+    public func get(for key: String) -> Swift.Result<Data, Error> {
+        do {
+            let data = try Disk.retrieve(path + "/\(key.md5)", from: directory, as: Data.self)
+            return .success(data)
+
+        } catch {
+            return .failure(Error(rawValue: (error as NSError).code) ?? .unknow)
+        }
+    }
+    
+    public func remove(forKey key: String) throws {
+        do {
+            try Disk.remove(path + "/\(key.md5)", from: directory)
+            
+        } catch {
+            throw Error(rawValue: (error as NSError).code) ?? .unknow
+        }
+    }
+    
+    public func clear() throws {
+        do {
+            try Disk.remove(path, from: directory)
+            
+        } catch {
+            throw Error(rawValue: (error as NSError).code) ?? .unknow
+        }
+    }
+}
+
+extension Cache {
+    
+    /// 缓存异常
+    public enum Error: Int, Swift.Error, LocalizedError {
+        case unknow = -1
+        case notFound = 0
+        case serialization = 1
+        case deserialization = 2
+        case invalidFileName = 3
+        case couldNotAccessTemporaryDirectory = 4
+        case couldNotAccessUserDomainMask = 5
+        case couldNotAccessSharedContainer = 6
+        
+        public var errorDescription: String? {
+            switch self {
+            case .notFound:
+                return "未找到文件"
+            case .serialization:
+                return "序列化失败"
+            case .deserialization:
+                return "反序列化失败"
+            case .invalidFileName:
+                return "无效文件名称"
+            case .couldNotAccessTemporaryDirectory:
+                return "无法访问临时目录"
+            case .couldNotAccessUserDomainMask:
+                return "无法访问用户域掩码"
+            case .couldNotAccessSharedContainer:
+                return "无法访问共享容器"
+            default:
+                return "未知"
+            }
+        }
+    }
+}
+
+public protocol Cacheable {
+}
+
+public extension Cacheable where Self: Codable {
+    
+    /// 缓存
+    /// - Parameter identifier: 标识
+    /// - Parameter cache: 缓存对象
+    func cache(identifier: CustomStringConvertible = "", from cache: Cache = .shared) throws {
+        try cache.set(self, forKey: "\(String(describing: Self.self))-\(identifier)")
+    }
+    
+    /// 获取
+    /// - Parameter identifier: 标识
+    /// - Parameter cache: 缓存对象
+    static func fetch(identifier: CustomStringConvertible = "", from cache: Cache = .shared) -> Swift.Result<Self, Cache.Error> {
+        cache.get(for: "\(String(describing: Self.self))-\(identifier)")
+    }
+    
+    /// 移除
+    /// - Parameter identifier: 标识
+    /// - Parameter cache: 缓存对象
+    static func remove(identifier: CustomStringConvertible = "", from cache: Cache = .shared) throws {
+        try cache.remove(forKey: "\(String(describing: Self.self))-\(identifier)")
+    }
+}
+
+fileprivate extension String {
+    
+    var md5: String {
+        guard let data = data(using: .utf8) else {
+            return self
+        }
+        
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        
+        _ = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
+            return CC_MD5(bytes.baseAddress, CC_LONG(data.count), &digest)
+        }
+        
+        return digest.map { String(format: "%02x", $0) }.joined()
+    }
+}
